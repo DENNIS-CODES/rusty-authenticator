65d8f67a99031ee2ed6d0d8693b63aec9a28b3b6
Dennis-Codes <dennis@ngeni.io>
2023-06-21 23:59:00 +0300

    added phone number in registration and added validation to kenyan phone number and added confirm password

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
+#![allow(dead_code)]
 use actix_web::{web, App, HttpResponse, HttpServer};
 use bcrypt::{hash, verify, DEFAULT_COST};
+use regex::Regex;
 use std::sync::Mutex;
 // In-memory storage for simplicity
 struct AppState {
     users: Mutex<Vec<User>>,
 }
-
 struct User {
     username: String,
     password_hash: String,
+    phone_number: String,
 }
 
 async fn register_user(data: web::Json<RegisterRequest>, state: web::Data<AppState>) -> HttpResponse {
@@ -19,16 +21,32 @@ async fn register_user(data: web::Json<RegisterRequest>, state: web::Data<AppSta
         return HttpResponse::Conflict().body("Username already taken");
     }
 
+    //validate phone number formart
+    let phone_number_regex = Regex::new(r"^(?:\+254|07|7)\d{8}$").unwrap();
+    if !phone_number_regex.is_match(&data.phone_number) {
+        return HttpResponse::BadRequest().body("Invalid phone number format");
+    }
+    // validate password
+    if data.password.len() < 8 || !data.password.chars().any(char::is_uppercase) ||  !data.password.chars().any(char::is_lowercase) || !data.password.chars().any(char::is_numeric) {
+        return HttpResponse::BadRequest().body("password must be at least 8 characters long , contain at least one uppercase lowercase letter and a digit");
+    }
     // Hash the password
     let password_hash = match hash(&data.password, DEFAULT_COST) {
         Ok(hash) => hash,
         Err(_) => return HttpResponse::InternalServerError().finish(),
     };
 
+    // check if the password is the same as the confirm password
+
+    if data.password != data.confirm_password {
+        return HttpResponse::BadRequest().body("password and confirm password do not match")
+    }
+
     // Create a new user
     let user = User {
         username: data.username.clone(),
         password_hash,
+        phone_number: data.phone_number.clone(),
     };
 
     // Store the user in the state
@@ -41,7 +59,7 @@ async fn login_user(data: web::Json<LoginRequest>, state: web::Data<AppState>) -
     let users = state.users.lock().unwrap();
 
     // Find the user by username
-    let user = match users.iter().find(|user| user.username == data.username) {
+    let user = match users.iter().find(|user| user.username == data.username || user.phone_number == data.username) {
         Some(user) => user,
         None => return HttpResponse::NotFound().body("User not found"),
     };
@@ -63,6 +81,8 @@ async fn login_user(data: web::Json<LoginRequest>, state: web::Data<AppState>) -
 struct RegisterRequest {
     username: String,
     password: String,
+    confirm_password: String,
+    phone_number: String,
 }
 
 #[derive(serde::Deserialize)]
